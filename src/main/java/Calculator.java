243dd12a16a818836f69a8da42806bdc88b46a2d
anomander96 <mister.truepaz@gmail.com>
2022-02-10 12:56:43 +0200

    created a Calculator for simple expressions

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,78 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Calculator {
+
+    private static final Stack<String> operations = new Stack<>();
+    private static final Stack<Integer> numbers = new Stack<>();
+
+
+    public static void main(String[] args) {
+        System.out.println("Please put an Integer number!");
+        System.out.println("For example : 1+1*(2-3)");
+        System.out.println("Enter your expression to get a solution: ");
+
+        try (Scanner sc = new Scanner(System.in)) {
+            while (sc.hasNextLine()) {
+                String expression = sc.nextLine().replaceAll("\\s", "");
+                calculate(expression);
+                System.out.println(expression + "=" + numbers.pop());
+            }
+        } catch (NoSuchElementException ex) {
+            System.err.println("Something went wrong: Please enter your expression!");
+        }
+    }
+
+    private static void calculate(String expression) {
+        int index = 0;
+
+        while (index < expression.length()) {
+            if (expression.charAt(index) == '(') {
+
+            } else if (expression.charAt(index) == '*' || expression.charAt(index) == '/' ||
+                    expression.charAt(index) == '+' || expression.charAt(index) == '-') {
+                operations.push(String.valueOf(expression.charAt(index)));
+            } else if (expression.charAt(index) == ')') {
+                doOperation();
+            } else {
+                StringBuilder value = new StringBuilder(String.valueOf(expression.charAt(index)));
+
+                while ((index + 1) < expression.length() &&
+                        Character.isDigit(expression.charAt(index + 1))) {
+                    value.append(expression.charAt(++index));
+                }
+                numbers.push(Integer.parseInt(value.toString()));
+            }
+            index++;
+        }
+
+        while (!operations.empty() && !numbers.empty()) {
+            doOperation();
+        }
+    }
+
+    private static void doOperation() {
+        String op = operations.pop();
+        switch (op) {
+            case "*":
+                numbers.push(numbers.pop() * numbers.pop());
+                break;
+            case "/":
+                try {
+                    numbers.push(numbers.pop() / numbers.pop());
+                } catch (ArithmeticException ex) {
+                    System.err.println("Your expression failed, you " + ex.getMessage());
+                }
+                break;
+            case "+":
+                numbers.push(numbers.pop() + numbers.pop());
+                break;
+            case "-":
+                numbers.push(numbers.pop() - numbers.pop());
+                break;
+            default:
+                System.out.println("You entered a wrong expression");
+        }
+    }
+}
