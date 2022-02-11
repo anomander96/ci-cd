19c13594fd686abfb09ff560d4f2ceba3e8c28f4
anomander96 <mister.truepaz@gmail.com>
2022-02-11 09:24:20 +0200

    added JenkinsFile

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+
+public class CalculatorTest {
+
+    @Test
+    public void testSuccess() {
+        Integer num = null;
+        assertNull(num);
+    }
+}
